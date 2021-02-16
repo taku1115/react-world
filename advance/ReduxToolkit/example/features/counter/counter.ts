494cb471ea8c937da2388fb56c815edc6a97b843
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-02-16 21:45:09 +0900

    ✏️ learn Redux Toolkit

---
+++ advance/ReduxToolkit/example/features/counter/counter.ts
@@ -0,0 +1,41 @@
+// createSliceでactionとreducerをまとめたもの
+
+import { createSlice, payloadAction } from '@reduxjs/toolkit';
+
+export type CounterState = {
+  count: number;
+};
+
+const initialState: CounterState = { count: 0 };
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reudcer: {
+    added: (state, action: payloadAction<number>) => ({
+      ...state,
+      count: state.count + action.payload,
+    }),
+    decremented: (state) => ({ ...state, count: state.count - 1 }),
+    incremented: (state) => ({ ...state, count: state.count + 1 }),
+  }
+});
+
+// actionとreducerを統合したものをRedux Toolkitでは「slice」と呼ぶ
+// createSliceを使うと、必然的にDucksパターンに準拠することになる
+// featuresディレクトリに、featureごとにsliceのファイルを置く
+// src/
+//  components/
+//    molecules/
+//      ColorfulBeads.tsx
+//    organisms/
+//      CounterBoard.tsx
+//  containers/
+//    molecules/
+//      ColorfulBeads.tsx
+//    organisms/
+//      CounterBoard.tsx
+//  features/
+//    counter.ts  
+//  App.tsx
+//  index.tsx
